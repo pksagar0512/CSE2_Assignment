27577d8c47c329976ee2d05cbd43f8b15af28c20
ayushmodi001 <2303031057006@paruluniversity.ac.in>
2025-02-27 20:06:34 +0530

    Added forgot password component with UI improvements

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { UserComponent } from './user/user.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent,RegisterComponent,UserComponent,ForgotPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
