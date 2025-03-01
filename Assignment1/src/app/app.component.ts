733ba05dd83a187c9d4337dadae63df557a1cf46
YashChudgar <yashchudgar2004@gmail.com>
2025-03-01 13:45:36 +0530

    Assignment-1 MEARN

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,12 +1,67 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { Component } from '@angular/core';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+// import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+// import { UserComponent } from './user/user.component';
+// import { EmployeeComponent } from './employee/employee.component';
+// import { AdminComponent } from './admin/admin.component';
+
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+//   imports:[LoginComponent,
+//     RegisterComponent,
+//     ForgotPasswordComponent,
+//     UserComponent,
+//     EmployeeComponent,
+//     AdminComponent]
+// })
+ 
+// // @NgModule({
+// //   declarations: [
+// //     AppComponent,
+// //     LoginComponent,
+// //     RegisterComponent,
+// //     ForgotPasswordComponent,
+// //     UserComponent,
+// //     EmployeeComponent,
+// //     AdminComponent
+// //   ],
+// //   imports: [BrowserModule],
+// //   providers: [],
+// //   bootstrap: [AppComponent]
+// // })
+// export class AppComponent { }
+// export class AppModule { }
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   currentComponent = 'login';
+// }
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, LoginComponent, RegisterComponent, ForgotPasswordComponent, UserComponent], // Add imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment1';
+  currentComponent = 'login'; // Default component
 }
