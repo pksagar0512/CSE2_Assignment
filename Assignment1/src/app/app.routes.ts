df718d2f538868fc6e73cf85e8b6b211adbb629b
JAYDEEPSOLANKI45 <jaydeepsolanki4567@gmail.com>
2025-03-01 14:51:17 +0530

    changes

--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    {
+        path:"about",
+        component:AboutComponent
+    },
+    {
+        path:"service",
+        component:ServiceComponent
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"contact",
+        component:ContactComponent
+    }
+];
